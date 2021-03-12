1174047c804c81c738f069a52bf1a727218c8ccb
Dawid <sgace2t1@gmail.com>
2021-03-12 17:58:30 +0100

    multiply, subtract methods added to Money class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -22,4 +22,12 @@ public class Money {
     public BigDecimal getAmount() {
         return amount;
     }
+
+    public Money multiply(BigDecimal multiplier) {
+        return new Money(currency, amount.multiply(multiplier));
+    }
+
+    public Money subtract(Money money) {
+        return new Money(currency, amount.subtract(money.getAmount()));
+    }
 }
